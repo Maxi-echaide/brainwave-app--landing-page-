487321ba35e00ab333edbf782055293d4cf017b8
Maxi-echaide <maxi.echaide@gmail.com>
2024-04-18 03:39:19 -0300

    Roadmap, footer plus tsome tweeks

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { notification1 } from "../assets";
 import { notificationImages } from "../constants";
 
 const Notification = ({ className, title }) => {
@@ -8,10 +8,17 @@ const Notification = ({ className, title }) => {
         className || ""
       } flex items-center p-4 pr-6 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl gap-5`}
     >
+      <img
+        src={notification1}
+        width={62}
+        height={62}
+        alt="image"
+        className="rounded-xl"
+      />
+
       <div className="flex-1">
-        <h6 className="mb-1 font-semibold justify-between text-base">
-          {title}
-        </h6>
+        <h6 className="mb-1 font-semibold text-base">{title}</h6>
+
         <div className="flex items-center justify-between">
           <ul className="flex -m-0.5">
             {notificationImages.map((item, index) => (
